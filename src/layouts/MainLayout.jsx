d9848e09c77cefc0baf4fdba0c0094131346268b
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-01-16 14:43:55 +0530

    Complete project

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import { Outlet } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/Navbar";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default MainLayout;
